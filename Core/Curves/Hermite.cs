9251186f260e34eed67381cdf57b535326562da1
zouhunter <1063627025@qq.com>
2017-12-31 21:53:47 +0800

    add a simple Hermite

---
+++ Core/Curves/Hermite.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace SplineCurve
+{
+
+    public class Hermite
+    {
+        private float d1;
+        private float d2;
+
+        public Hermite(float d1, float d2)
+        {
+            this.d1 = d1;
+            this.d2 = d2;
+        }
+
+        public float GetLine(float p1, float p2, float t)
+        {
+            return
+                p1 * MuitCalc(new float[] { 2, -3, 1 }, new float[] { 3, 2, 0 }, t) +
+                p2 * MuitCalc(new float[] { -2, 3 }, new float[] { 3, 2 }, t) +
+                d1 * MuitCalc(new float[] { 1, -2, 1 }, new float[] { 3, 2, 1 }, t) +
+                d2 * MuitCalc(new float[] { 1, -1 }, new float[] { 3, 2 }, t);
+
+        }
+        private float MuitCalc(float[] arg, float[] pow, float value)
+        {
+            Debug.Assert(pow.Length == arg.Length);
+            var result = 0f;
+            for (int i = 0; i < arg.Length; i++)
+            {
+                result += arg[i] * Mathf.Pow(value, pow[i]);
+            }
+            return result;
+        }
+    }
+}
---
+++ Demo/Hermite/HermiteTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SplineCurve;
+[ExecuteInEditMode]
+public class HermiteTest : MonoBehaviour {
+    // Use this for initialization
+    public int count = 10;
+    public LineRenderer lineRender;
+    public Transform p1;
+    public Transform p2;
+
+    private Hermite xH;
+    private Hermite yH;
+    private Hermite zH;
+
+    private Vector3 posTemp;
+    void Start () {
+        xH = new Hermite(1, 1);
+        yH = new Hermite(1, 1);
+        zH = new Hermite(1, 1);
+        lineRender.positionCount = count;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < count; i++)
+        {
+            posTemp.x = xH.GetLine(p1.position.x, p2.position.x, i / (count - 1f));
+            posTemp.y = yH.GetLine(p1.position.y, p2.position.y, i / (count - 1f));
+            posTemp.z = zH.GetLine(p1.position.z, p2.position.z, i / (count - 1f));
+            lineRender.SetPosition(i, posTemp);
+        }
+
+    }
+}
